6f0aa4d47134b4461990cc74c10c2f47f8f8c53b
irgifahlevi <irgifahlevi5@gmail.com>
2024-02-16 21:09:59 +0700

    Add service registration and implements EmployeeRepository

---
+++ EmployeeManagement.Presistance/PresistenceServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Application.Presistance.Contracts;
+using EmployeeManagement.Presistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Presistance
+{
+    public static class PresistenceServiceRegistration
+    {
+        private static string _connectionString;
+        public static IServiceCollection ConfigurePresistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("EmployeeManagement");
+            services.AddDbContext<EmployeeDbContext>(options => 
+                options.UseSqlServer(_connectionString));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ EmployeeManagement.Presistance/Repositories/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagement.Application.Presistance.Contracts;
+using EmployeeManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Presistance.Repositories
+{
+    public class EmployeeRepository : GenericRepository<Employee>, IEmployeeRepository
+    {
+        private readonly EmployeeDbContext _dbContext;
+
+        public EmployeeRepository(EmployeeDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // Implement another method here
+    }
+}
