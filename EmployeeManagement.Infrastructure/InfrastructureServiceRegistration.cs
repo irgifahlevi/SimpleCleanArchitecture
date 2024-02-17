f006e1f4141039fb3cecaadc271e4059c66bf5d5
irgifahlevi <irgifahlevi5@gmail.com>
2024-02-17 13:23:51 +0700

    Implement EmailSender, configuration service infrastructure add API employee management

---
+++ EmployeeManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.Application.Contracts.Infastructure;
+using EmployeeManagement.Application.Models;
+using EmployeeManagement.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        private static IConfiguration _emailSettings;
+        public static IServiceCollection ConfigurationInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            _emailSettings = configuration.GetSection("EmailSettings");
+            services.Configure<EmailSettings>(_emailSettings);
+            services.AddTransient<IEmailSender, EmailSender>();
+            return services;
+        }
+    }
+}
---
+++ EmployeeManagement.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,45 @@
+﻿using EmployeeManagement.Application.Contracts.Infastructure;
+using EmployeeManagement.Application.Models;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Infrastructure.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        private EmailSettings _emailSetings { get; }
+
+        public EmailSender(IOptions<EmailSettings> emailSetings)
+        {
+            _emailSetings = emailSetings.Value;
+        }
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_emailSetings.ApiKey);
+            var to = new EmailAddress(email.To);
+            var form = new EmailAddress
+            { 
+                Email = _emailSetings.FromAddress,
+                Name = _emailSetings.FromName
+            };
+
+            var message = MailHelper.CreateSingleEmail(form, to, email.Subject, email.Body, email.Body);
+
+            var response = await client.SendEmailAsync(message);
+
+            if(response.IsSuccessStatusCode) 
+            {
+                return response.StatusCode == HttpStatusCode.OK;
+            }
+
+            return false;
+        }
+    }
+}
