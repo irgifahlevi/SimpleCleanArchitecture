ff7c16e4c0ad91204b616f918457ba844c3d6d82
irgifahlevi <irgifahlevi5@gmail.com>
2024-02-17 13:41:54 +0700

    Configure all services application, infrastructure, presistence and add policy

--- EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/Program.cs
@@ -1,11 +1,27 @@
+using EmployeeManagement.Application;
+using EmployeeManagement.Infrastructure;
+using EmployeeManagement.Presistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigurationInfrastructureServices(builder.Configuration);
+builder.Services.ConfigurePresistenceServices(builder.Configuration);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
